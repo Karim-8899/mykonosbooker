2da60b7179d8902759064257eaaac0365e735968
Karim-9 <125803734+Karim-8899@users.noreply.github.com>
2023-04-12 04:07:48 +0200

    update  the search top main bar
    update  the search top main bar

--- js/custom.js
+++ js/custom.js
@@ -3631,21 +3631,6 @@ function showRoom4() {
 		}
 		
 
-		
-// ---- ---- Const ---- ---- //
-let inputBox = document.querySelector('.input-box'),
-  searchIcon = document.querySelector('.searchbar'),
-  closeIcon = document.querySelector('.close-icon');
-
-// ---- ---- Open Input ---- ---- //
-searchIcon.addEventListener('click', () => {
-  inputBox.classList.add('open');
-});
-// ---- ---- Close Input ---- ---- //
-closeIcon.addEventListener('click', () => {
-  inputBox.classList.remove('open');
-});
-
 
 
 
