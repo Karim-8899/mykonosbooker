9101f0c792d64076e03ac67f40271989ff77f7d4
Karim-9 <125803734+Karim-8899@users.noreply.github.com>
2023-05-19 14:35:28 +0300

    Change on Main Mykonos
    Header Mykonos Font changes
    'byound' to minimal

--- js/custom.js
+++ js/custom.js
@@ -3438,6 +3438,19 @@ window.location.href = `https://bookings.mykonosbooker.com/#/search/${year}~2F${
 
 })
 
+function validateForm() {
+    var name = document.getElementById("roundedk-in").value;
+    var email = document.getElementById("roundedk-out").value;
+
+    if (name === "" || email === "") {
+      alert("Please fill out all fields");
+      return false; // Prevent form submission
+    }
+
+    // Proceed with form submission
+    return true;
+  }
+
 
 
 
