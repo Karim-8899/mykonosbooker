09db6bd251a5d6bf796e53b23fa3fc1e6ae76e29
Karim-9 <125803734+Karim-8899@users.noreply.github.com>
2023-05-27 06:04:27 +0300

    update phpmailer
    update phpmailer + Autoload

---
+++ phpmailer.php
@@ -0,0 +1,50 @@
+
+
+<?php
+   
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+   use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/PHPMailerAutoload.php';
+
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+
+   $mail = new PHPMailer();
+   $mail->isSMTP();
+   $mail->Host = 'smtp.hostinger.com';
+   $mail->Port = 465;
+   $mail->SMTPAuth = true;
+   $mail->Username = 'no-reply@mykonosbooker.com';
+   $mail->Password = 'Nomykonos11!';
+   $mail->SMTPSecure = 'ssl';
+   
+   $mail->setFrom('no-reply@mykonosbooker.com', 'Mykonos Booker');
+   $mail->addAddress(`$email`, `$name`);
+   $mail->Subject = 'Welcome to MykonosBooker';
+   $mail->Body = 'Thank you For Contacting MykonosBooker';
+   //$mail->msgHTML(file_get_contents('message.html'), __DIR__);
+  // $mail->addReplyTo('info@mykonosbooker.com', 'Mykonos Booker');
+   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+   
+   //$mail->addAttachment('attachment.txt');
+   
+  if(!$mail->send()) {
+    echo 'Email could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo '<center><h1>Email has been sent.</h1></center>';
+}
+
+
+
+
+
+
+?>
